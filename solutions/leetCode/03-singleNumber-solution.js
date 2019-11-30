81088470934d5d356b18e3485ecbb745d0286242
Ken S <gxcadr@gmail.com>
2019-11-29 22:42:42 -0800

    solution for singleNumber

--- solutions/leetCode/03-singleNumber-solution.js
+++ solutions/leetCode/03-singleNumber-solution.js
@@ -3,13 +3,49 @@
  * @return {number}
  */
 
-// If you see this comment, solution is still incomplete
 const singleNumber = function(nums) {
-  const tracker = {};
+  const numSet = new Set();
 
   nums.forEach((num) => {
-    if (tracker[num] !== undefined) {
-      tracker[num] = (tracker[num] || 0) += 1;
+    if (numSet.has(num)) {
+      numSet.delete(num);
+    } else {
+      numSet.add(num);
     }
-  })
+  });
+
+  return parseInt(...numSet, 10);
+  // return numSet.values().next().value;
 };
+
+const singleNumberMath = function(nums) {
+  const numSet = new Set(nums);
+
+  let total = 0;
+  numSet.forEach((num) => {
+    total += num;
+  });
+
+  total *= 2;
+
+  const sumOfWholeArray = nums.reduce((acc, num) => acc + num);
+
+  return total - sumOfWholeArray;
+};
+
+// O(n) time
+// O(1) space
+
+const singleNumberXor = (nums) => {
+  return nums.reduce((acc, num) => acc ^ num);
+};
+
+const hugeArray = [];
+
+for (let i = 5; i < 5000000; i += 1) {
+  hugeArray.push(i);
+  hugeArray.push(i);
+}
+hugeArray.push(3);
+
+console.log(singleNumberXor(hugeArray)); // --> 3
