93c7e76fec537869a10e2197234d971446025c29
Ken S <gxcadr@gmail.com>
2019-12-09 11:19:47 -0800

    problem added 07-longestCommonPrefix

---
+++ Problems/leetCode/07-longestCommonPrefix.js
@@ -0,0 +1,9 @@
+/*
+Given an array of strings, return the longest common prefix.
+
+e.g. const sampleArray = ['abcdef', 'abcghi', 'abcabc'] --> 'abc'
+*/
+
+const longestCommonPrefix = function (arrayOfStrings) {
+
+};
