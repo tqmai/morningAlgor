e310ea28559d356daee09487f754c5e1435200b0
Timothy Mai <timothy.mai13@gmail.com>
2019-12-11 00:17:48 -0800

    added iterative solution to invertBinaryTree

--- Problems/leetCode/08-invertBinaryTree.js
+++ Problems/leetCode/08-invertBinaryTree.js
@@ -47,3 +47,45 @@ const invertTree = function(root) {
 
     return root;
 };
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTreeIterative = function(root) {
+  // if root doesn't exist, then return null
+  if (!root) {
+    return null;
+  }
+
+  // create a queue to determine what nodes to look at 
+  const queue = [];
+
+  // add root to the queue
+  queue.push(root);
+
+  // do this while queue is not empty
+  // when it is empty, we will have gone through the entire tree
+  while (queue.length !== 0) {
+    // make pointer to current node
+    const pointer = queue[0];
+    
+    // swap the left and right sides of the current node
+    [pointer.left, pointer.right] = [pointer.right, pointer.left];
+
+    // add the left side to the queue if it is not null
+    if (pointer.left) {
+      queue.push(pointer.left);
+    }
+
+    // add the right side to the queue if it is not null
+    if (pointer.right) {
+      queue.push(pointer.right);
+    }
+
+    // remove the current node from the queue
+    queue.shift();
+  }
+
+  return root;
+}
