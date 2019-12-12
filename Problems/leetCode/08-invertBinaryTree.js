91291b3ccb1bc0a49e91b10c2eab3ce0b60a0e33
Ken S <gxcadr@gmail.com>
2019-12-11 21:15:24 -0800

    minor changes to 08 problem, and uploaded solution

--- Problems/leetCode/08-invertBinaryTree.js
+++ Problems/leetCode/08-invertBinaryTree.js
@@ -21,17 +21,16 @@ Output:
 9   6 3   1
 */
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+//  Definition for a binary tree node.
+// function TreeNode(val) {
+//   this.val = val;
+//   this.left = null;
+//   this.right = null;
+// }
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
  */
 const invertTree = function(root) {
-    
+  
 };
