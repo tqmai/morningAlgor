31598091b0d6991a753ea15953b51a6ca3558ec6
Ken S <gxcadr@gmail.com>
2019-12-17 11:37:52 -0800

    minor update to problem 12

--- Problems/leetCode/12-findSpecialInteger.js
+++ Problems/leetCode/12-findSpecialInteger.js
@@ -1,6 +1,5 @@
 /* https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
 1287. Element Appearing More Than 25% In Sorted Array
-Easy
 
 Given an integer array sorted in non-decreasing order, there is exactly one integer
 in the array that occurs more than 25% of the time.
