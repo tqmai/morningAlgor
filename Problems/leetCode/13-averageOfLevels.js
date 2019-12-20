d7248cf957a9c1ee4e90faefdb419cd1884b42a3
Ken S <gxcadr@gmail.com>
2019-12-19 22:04:28 -0800

    minor changes to problem 13

--- Problems/leetCode/13-averageOfLevels.js
+++ Problems/leetCode/13-averageOfLevels.js
@@ -16,17 +16,27 @@ The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 
 return [3, 14.5, 11].
 */
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+
+//  Definition for a binary tree node.
+function TreeNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
+
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
-const averageOfLevels = function(root) {
+
+const averageOfLevels = function (root) {
   
 };
+
+// const sampleTree = new TreeNode(3);
+// sampleTree.left = new TreeNode(9);
+// sampleTree.right = new TreeNode(20);
+// sampleTree.right.left = new TreeNode(15);
+// sampleTree.right.right = new TreeNode(7);
+
+// console.log(averageOfLevels(sampleTree));
