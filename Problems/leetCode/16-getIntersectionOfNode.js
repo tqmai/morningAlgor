32e87519941c8f9c5a1d9c136d0f188c4df56e5e
Ken S <gxcadr@gmail.com>
2019-12-27 09:44:25 -0800

    slight update to problem 16

--- Problems/leetCode/16-getIntersectionOfNode.js
+++ Problems/leetCode/16-getIntersectionOfNode.js
@@ -33,19 +33,21 @@ Notes:
     Your code should preferably run in O(n) time and use only O(1) memory.
     */
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
+
+// Definition for singly-linked list.
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
 
 /**
  * @param {ListNode} headA
  * @param {ListNode} headB
  * @return {ListNode}
  */
-const getIntersectionNode = function(headA, headB) {
+
+
+const getIntersectionNode = function (headA, headB) {
   
 };
