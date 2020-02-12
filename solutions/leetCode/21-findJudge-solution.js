dafe9acd795999a237c7eaccca0907a099a1c07e
Ken S <gxcadr@gmail.com>
2020-02-11 20:57:40 -0800

    added alternative syntax solution for 21-findJudge

--- solutions/leetCode/21-findJudge-solution.js
+++ solutions/leetCode/21-findJudge-solution.js
@@ -86,10 +86,35 @@ const findJudge = function (N, trust) {
   return -1;
 };
 
-const test1 = [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]];
-const test2 = [[1, 3], [2, 3], [3, 1]];
+// similar logic, cleaner/alternative syntax
+const findJudgeAlternative = function (N, trust) {
+  const truster = new Set();
+  const trusted = [];
 
-console.log(findJudge(3, test2));
+  trust.forEach((numSet) => {
+    let [a, b] = numSet;
+    a -= 1;
+    b -= 1;
+    truster.add(a);
+    trusted[b] = (trusted[b] || 0) + 1;
+  });
+
+  for (let i = 0; i < N; i += 1) {
+    if (!truster.has(i) && trusted[i] === N - 1) {
+      return i + 1;
+    }
+  }
+
+  return -1;
+};
+
+const test1 = [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]; // 3
+const test2 = [[1, 3], [2, 3], [3, 1]]; // -1
+const test3 = [[1, 2]]; // 2
+
+console.log(findJudgeAlternative(4, test1));
+console.log(findJudgeAlternative(3, test2));
+console.log(findJudgeAlternative(2, test3));
 
 // every other number has to trust one number
 // the judge number cannot ever be the fist number in a sub array
